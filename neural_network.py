a98a0ae3bb1a7a83c943e73f38fa0a792ed86df3
Toby Courtis <toby.courtis@hotmail.com>
2019-12-04 12:47:13 +0000

    Added reading in data and training of NN section

---
+++ neural_network.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+
+class MyNetwork(nn.Module):
+    def __init__(self):
+        super(MyNetwork, self).__init__()
+        layers = nn.ModuleList()
+        layers.append(nn.Linear(in_features=3*277*277, out_features=512))
+        layers.append(nn.ReLU())
+        layers.append(nn.Linear(in_features=512, out_features=32))
+        layers.append(nn.ReLU())
+        layers.append(nn.Linear(in_features=32, out_features=512))
+        layers.append(nn.ReLU())
+        layers.append(nn.Linear(in_features=512, out_features=3*277*277))
+        layers.append(nn.Sigmoid())
+        self.layers = layers
+
+    def forward(self, x):
+        z = self.encode(x)
+        x = self.decode(z)
+        return x
+
+    # encode (flatten as linear, then run first half of network)
+    def encode(self, x):
+        x = x.view(x.size(0), -1)
+        for i in range(4):
+            x = self.layers[i](x)
+        return x
+
+    # decode (run second half of network then unflatten)
+    def decode(self, x):
+        for i in range(4,8):
+            x = self.layers[i](x)
+        x = x.view(x.size(0), 3, 277, 277)
+        return x
--- train_classifier.py
+++ train_classifier.py
@@ -4,7 +4,9 @@ import torch.utils.data as utils
 import cv2
 import os
 import random
+import numpy as np
 from local_classes import Dataset
+from neural_network import MyNetwork
 
 ########
 #print("This is for training the classifier based off my university project")
@@ -15,35 +17,91 @@ from local_classes import Dataset
 
 
 # Section 1 - Iterate directory and create labels and data lists
-training_data_path = "../face_detector_data"
-dataset = []
-labels = []
+def generate_dataset():
+    training_data_path = "../face_detector_data"
+    dataset = []
+    labels = []
 
-for dirname, dirnames, filenames in os.walk(training_data_path):
-    for filename in filenames:
-        if((filename.find(".png") > -1) | (filename.find(".jpg") > -1)):
-            name = str(dirname.split('/')[-1])
-            read_image = cv2.imread(dirname+'/'+filename, cv2.COLOR_BGR2GRAY)
-            resized_read_image = cv2.resize(read_image, (273, 273), interpolation = cv2.INTER_AREA)
-            dataset.append(resized_read_image)
-            labels.append(name)
+    for dirname, dirnames, filenames in os.walk(training_data_path):
+        for filename in filenames:
+            if((filename.find(".png") > -1) | (filename.find(".jpg") > -1)):
+                name = str(dirname.split('/')[-1])
+                read_image = cv2.imread(dirname+'/'+filename, cv2.COLOR_BGR2GRAY)
+                resized_read_image = cv2.resize(read_image, (273, 273), interpolation = cv2.INTER_AREA)
+                dataset.append(resized_read_image)
+                labels.append(name)
 
-#shuffle lists and dataset/labels are now shuffled in the same order dataset'img' == labels'img label'
-z = list(zip(dataset, labels))
-random.shuffle(z)
-dataset, labels = zip(*z)
+    #shuffle lists and dataset/labels are now shuffled in the same order dataset'img' == labels'img label'
+    z = list(zip(dataset, labels))
+    random.shuffle(z)
+    dataset, labels = zip(*z)
+    return dataset, labels
 
+generated = generate_dataset()
+dataset = generated[0]
+labels  =  generated[1]
+print("> Datasets genereated")
+print(len(dataset))
+print(len(labels))
 # initialise training dataset // could make the above functions and save to pkl if needed
 training_set = Dataset(dataset, labels)
 
+parameters = {'batch_size': 16, 'shuffle': False, 'num_workers': 6}
+training_generator = utils.DataLoader(training_set, **parameters)
+
+def cycle(iterable):
+    while True:
+        for x in iterable:
+            yield x
+
+train_iterator = iter(cycle(training_generator))
 
 
+# Section 2 - Input data into NN from neural_network.py and train
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+N = MyNetwork().to(device)
 
-# Section 2 - Input data into CNN and TRAIN
+print("> Number of network parameters {}".format(len(torch.nn.utils.parameters_to_vector(N.parameters()))))
 
+# initialise the optimiser
+optimiser = torch.optim.Adam(N.parameters(), lr=0.001)
 
+epoch = 0
+max_epochs = 5
+print("> Training NN")
+while(epoch < max_epochs):
+    # arrays for metrics
+    logs = {}
+    train_loss_arr = np.zeros(0)
+    valid_loss_arr = np.zeros(0)
 
+    # iterate over some of the train dateset
+    for i in range(1000):
+        # x,t = next(train_iterator)
+        # print(x,t)
+        # x,t = x.to(device), t.to(device)
+        for x, t in training_generator:
+            x, t = x.to(device), t.to(device)
 
+        optimiser.zero_grad()
+        p = N(x)
+        loss = ((p-x)**2).mean() # simple l2 loss
+        loss.backward()
+        optimiser.step()
+
+        train_loss_arr = np.append(train_loss_arr, loss.cpu().data)
+
+    # NOTE: live plot library has dumb naming forcing our 'test' to be called 'validation'
+    # liveplot.update({
+    #     'loss': train_loss_arr.mean()
+    # })
+    # liveplot.draw()
+
+    epoch = epoch+1
+    print ("Epoch:", epoch, "Training Loss: ", np.mean(train_loss_arr))
+    # print ("Epoch:", epoch, "Training Loss: ", np.mean(train_loss_arr), "Valid Loss: ", np.mean(valid_loss))
+
+print("Finished training Neural Network")
 
 
 
@@ -57,11 +115,14 @@ training_set = Dataset(dataset, labels)
 # print("Finished saving Classifier...")
 
 
-
 path = "../face_detector_data/toby/toby_face0.png"
 image = cv2.imread(path, cv2.COLOR_BGR2GRAY)
-scale = 25
+image_tensor = torch.from_numpy(image)
+prediction = N(image_tensor)
 
+print(prediction)
+
+scale = 25
 while(False):
     #cv2.imshow("test", image)
     width = int(image.shape[1] * (scale/100))
